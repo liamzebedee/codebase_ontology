f8d2ac8643b65bf2f32b1e9edf8516b5b34104a0
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2017-02-09 23:48:54 +1100

    mmmm

--- analyse.py
+++ analyse.py
@@ -42,8 +42,8 @@ class Parser(object):
 		except Exception as err:
 			print("Error parsing %s" % name)
 
-
-		print(nicedump(tree))
+		# print(nicedump(tree))
+		process_ast(tree)
 
 
 def astdump(node, annotate_fields=True, include_attributes=False):
@@ -90,7 +90,7 @@ def get_name_for_node(node):
 	# Globals/nonlocals
 	elif isinstance(node, ast.Global) or isinstance(node, ast.Nonlocal):
 		# for nname in node.names:
-		print("UMMMM")
+		pass
 
 	# Attributes
 	elif isinstance(node, ast.Attribute):
@@ -117,20 +117,30 @@ def get_name_for_node(node):
 
 	return name
 
-def process_ast(ast):
-	if not isinstance(node, ast.AST):
-		raise TypeError('expected AST, got %r' % node.__class__.__name__)
+def process_ast(tree):
+	if not isinstance(tree, ast.AST):
+		raise TypeError('expected AST, got %r' % tree.__class__.__name__)
 
-	return process(ast, parent_named_node=None)
+	return process(tree, parent_named_node=None)
 
-# def process(node, parent_named_node=None):
-# 	if isinstance(node, ast.AST):
-# 		# fields = [(a, _process(b, tablevel+1)) for a, b in ast.iter_fields(node) if a in interesting_attrs]
-# 		# field for field in fields
-# 		return rv
-# 	elif isinstance(node, list):
-# 		return ('[%s\n'+indent+']') % '\n'.join(_process(x, tablevel+1) for x in node)
-# 	return repr(node)
+def process(node, parent_named_node=None):
+	this_named_node = get_name_for_node(node)
+	if not this_named_node:
+		 this_named_node = parent_named_node
+	else:
+		print(this_named_node)
+	children = get_children_for_node(node)
+	if children:
+		for child in get_children_for_node(node):
+			process(child, parent_named_node=this_named_node)
+	# if isinstance(node, ast.AST):
+	# 	# fields = [(a, _process(b, tablevel+1)) for a, b in ast.iter_fields(node) if a in interesting_attrs]
+	# 	# field for field in fields
+	# 	return rv
+	# elif isinstance(node, list):
+	# 	return ('[%s\n'+indent+']') % '\n'.join(_process(x, tablevel+1) for x in node)
+	# return repr(node)
+
 def get_children_for_node(node):
 	# for each attribute
 	# if attribute is not None
@@ -141,57 +151,60 @@ def get_children_for_node(node):
 	#   if attribute is singular
 	children = []
 	if isinstance(node, ast.Module):
-		children = node['body']
+		children = node.body
 	if isinstance(node, ast.Expression):
-		children = [node['body']]
+		children = [node.body]
 	if isinstance(node, ast.FunctionDef) or isinstance(node, ast.AsyncFunctionDef):
-		children.append(node['args'])
-		children.extend(node['body'])
-		if node['returns']:
-			children.append(node['returns'])
+		children.append(node.args)
+		children.extend(node.body)
+		if node.returns:
+			children.append(node.returns)
 	if isinstance(node, ast.ClassDef):
-		children.extend(node['bases'])
-		children.extend(node['keywords'])
-		children.extend(node['body'])
+		children.extend(node.bases)
+		children.extend(node.keywords)
+		children.extend(node.body)
 	if isinstance(node, ast.Return):
-		if node['value']:
-			children.append(node['value'])
+		if node.value:
+			children.append(node.value)
 	if isinstance(node, ast.Delete):
-		children.extend(node['targets'])
-	if isinstance(node, ast.Assign) or isinstance(node, ast.AugAssign) or isinstance(node, ast.AnnAssign):
-		children.extend(node['targets'])
-		children.append(node['value'])
+		children.extend(node.targets)
+	if isinstance(node, ast.Assign):# or isinstance(node, ast.AnnAssign):
+		children.extend(node.targets)
+		children.append(node.value)
+	if isinstance(node, ast.AugAssign):
+		children.append(node.target)
+		children.append(node.value)
 	if isinstance(node, ast.For) or isinstance(node, ast.AsyncFor):
 		# (expr target, expr iter, stmt* body, stmt* orelse)
-		children.append(node['target'])
-		children.extend(node['iter'])
-		children.extend(node['body'])
-		children.extend(node['orelse'])
+		children.append(node.target)
+		children.extend(node.iter)
+		children.extend(node.body)
+		children.extend(node.orelse)
 	if isinstance(node, ast.While):
-		children.append(node['test'])
-		children.extend(node['body'])
-		children.extend(node['orelse'])
+		children.append(node.test)
+		children.extend(node.body)
+		children.extend(node.orelse)
 	if isinstance(node, ast.If):
 		# expr test, stmt* body, stmt* orelse
-		children.append(node['test'])
-		children.extend(node['body'])
-		children.extend(node['orelse'])
+		children.append(node.test)
+		children.extend(node.body)
+		children.extend(node.orelse)
 	if isinstance(node, ast.With) or isinstance(node, ast.AsyncWith):
 		# withitem* items, stmt* body)
-		children.extend(node['items'])
-		children.extend(node['body'])
+		children.extend(node.items)
+		children.extend(node.body)
 	if isinstance(node, ast.Raise):
 		# (expr? exc, expr? cause)
-		if(node['exc']):
-			children.append(node['exc'])
-		if(node['cause']):
-			children.append(node['cause'])
+		if(node.exc):
+			children.append(node.exc)
+		if(node.cause):
+			children.append(node.cause)
 	if isinstance(node, ast.Try):
 		# (stmt* body, excepthandler* handlers, stmt* orelse, stmt* finalbody)
-		children.extend(node['body'])
-		children.extend(node['handlers'])
-		children.extend(node['orelse'])
-		children.extend(node['finalbody'])
+		children.extend(node.body)
+		children.extend(node.handlers)
+		children.extend(node.orelse)
+		children.extend(node.finalbody)
 	if isinstance(node, ast.Assert):
 		# (expr test, expr? msg)
 		pass
@@ -209,7 +222,7 @@ def get_children_for_node(node):
 		pass
 
 	if isinstance(node, ast.Expr):
-		children.append(node['value'])
+		children.append(node.value)
 
 	if isinstance(node, ast.Pass) or isinstance(node, ast.Break) or isinstance(node, ast.Continue):
 		pass
@@ -217,62 +230,62 @@ def get_children_for_node(node):
 
 	if isinstance(node, ast.BoolOp):
 		# values
-		children.extend(node['values'])
+		children.extend(node.values)
 	if isinstance(node, ast.BinOp):
-		children.extend([node['right'], node['left']])
+		children.extend([node.right, node.left])
 	if isinstance(node, ast.UnaryOp):
 		# operand
-		children.append(node['operand'])
+		children.append(node.operand)
 	if isinstance(node, ast.Lambda):
 		# arguments args, expr body
 		childen.append('args')
 		childen.append('args')
 	if isinstance(node, ast.IfExp):
 		# expr test, expr body, expr orelse
-		children.append(node['test'])
-		children.append(node['body'])
-		children.append(node['orelse'])
+		children.append(node.test)
+		children.append(node.body)
+		children.append(node.orelse)
 	if isinstance(node, ast.Dict):
 		# (expr* keys, expr* values)
-		children.extend(node['keys'])
-		children.extend(node['values'])
+		children.extend(node.keys)
+		children.extend(node.values)
 	if isinstance(node, ast.Set):
 		# Set(expr* elts)
-		children.extend(node['elts'])
+		children.extend(node.elts)
 	if isinstance(node, ast.ListComp) or isinstance(node, ast.SetComp):
 		# expr elt, comprehension* generators
-		children.append(node['elt'])
-		children.extend(node['generators'])
+		children.append(node.elt)
+		children.extend(node.generators)
 	if isinstance(node, ast.DictComp):
 		# expr key, expr value, comprehension* generators)
-		children.append(node['value'])
-		children.extend(node['generators'])
+		children.append(node.value)
+		children.extend(node.generators)
 	if isinstance(node, ast.GeneratorExp):
 		# (expr elt, comprehension* generators)
-		children.append(node['elt'])
-		children.extend(node['generators'])
+		children.append(node.elt)
+		children.extend(node.generators)
 	if isinstance(node, ast.Await) or isinstance(node, ast.YieldFrom):
 		# value
-		children.append(node['value'])
+		children.append(node.value)
 	if isinstance(node, ast.Yield):
 		# value?
-		if node['value']:
-			children.append(node['value'])
+		if node.value:
+			children.append(node.value)
 	if isinstance(node, ast.Compare):
 		# expr left, cmpop* ops, expr* comparators)
-		children.append(node['left'])
-		children.extend(node['comparators'])
+		children.append(node.left)
+		children.extend(node.comparators)
 	if isinstance(node, ast.Call):
 		# expr func, expr* args, keyword* keywords)
-		children.append(node['func'])
-		children.extend(node['args'])
-		children.extend(node['keywords'])
-	if isinstance(node, ast.FormattedValue):
+		children.append(node.func)
+		children.extend(node.args)
+		children.extend(node.keywords)
+	# if isinstance(node, ast.FormattedValue):
 		# FormattedValue(expr value, int? conversion, expr? format_spec)
-		children.append(node['value'])
-	if isinstance(node, ast.JoinedStr):
+		# children.append(node.value)
+	# if isinstance(node, ast.JoinedStr):
 		# values
-		children.extend(node['values'])
+		# children.extend(node.values)
 
 
 	# Primitives
@@ -284,7 +297,7 @@ def get_children_for_node(node):
 	 # | Constant(constant value)
 
 	if isinstance(node, ast.Attribute) or isinstance(node, ast.Subscript) or isinstance(node, ast.Starred):
-		children.append(node['value'])
+		children.append(node.value)
 		# (expr value, identifier attr, expr_context ctx)
 		# Subscript(expr value, slice slice, expr_context ctx)
 		# | Starred(expr value, expr_context ctx)
@@ -293,7 +306,7 @@ def get_children_for_node(node):
 	# | Name(identifier id, expr_context ctx)
 	if isinstance(node, ast.List) or isinstance(node, ast.Tuple):
 		# (expr* elts, expr_context ctx)
-		children.extend(node['elts'])
+		children.extend(node.elts)
 
 	if isinstance(node, ast.Slice):
 		# slice = Slice(expr? lower, expr? upper, expr? step)
@@ -302,7 +315,7 @@ def get_children_for_node(node):
 	  # | ExtSlice(slice* dims)
 	  pass
 	if isinstance(node, ast.Index):
-		children.append(node['value'])
+		children.append(node.value)
 
 	# boolop = And | Or
 
@@ -315,27 +328,27 @@ def get_children_for_node(node):
 
 	if isinstance(node, ast.comprehension):
 		# (expr target, expr iter, expr* ifs, int is_async)
-		children.append(node['target'])
-		children.append(node['iter'])
-		children.extend(node['ifs'])
+		children.append(node.target)
+		children.append(node.iter)
+		children.extend(node.ifs)
 
 	if isinstance(node, ast.excepthandler):
-		if node['type']:
-			children.append(node['type'])
-		children.extend(node['body'])
+		if node.type:
+			children.append(node.type)
+		children.extend(node.body)
 		# excepthandler = ExceptHandler(expr? type, identifier? name, stmt* body)
 		# attributes (int lineno, int col_offset)
 	if isinstance(node, ast.arguments):
-		children.extend(node['args'])
-		children.extend(node['kwonlyargs'])
+		children.extend(node.args)
+		children.extend(node.kwonlyargs)
 
-		if node['vararg']:
-			children.append(node['vararg'])
-		if node['kwarg']:
-			children.append(node['kwarg'])
+		if node.vararg:
+			children.append(node.vararg)
+		if node.kwarg:
+			children.append(node.kwarg)
 		
-		children.extend(node['kw_defaults'])
-		children.extend(node['defaults'])
+		children.extend(node.kw_defaults)
+		children.extend(node.defaults)
 
 		# (arg* args, arg? vararg, arg* kwonlyargs, expr* kw_defaults,
 			 # arg? kwarg, expr* defaults)
@@ -345,7 +358,7 @@ def get_children_for_node(node):
 		# attributes (int lineno, int col_offset)
 	if isinstance(node, ast.keyword):
 		# (identifier? arg, expr value)
-		children.append(node['value'])
+		children.append(node.value)
 
 	# TODO will have to handle this.
 	if isinstance(node, ast.alias):
@@ -355,13 +368,16 @@ def get_children_for_node(node):
 
 	if isinstance(node, ast.withitem):
 		# (expr context_expr, expr? optional_vars)
-		children.append(node['context_expr'])
-		if node['optional_vars']:
-			children.append(node['optional_vars'])
+		children.append(node.context_expr)
+		if node.optional_vars:
+			children.append(node.optional_vars)
 
+	return children
+
+identifier_field_names = ['id', 'name', 'module', 'attr', 'arg', 'asname']
 def nicedump2(tree):
 	def recurse(node):
-		ids = [id_ for _, b in ast.iter_fields(node) if isinstance(b, ast.Identifier)]
+		ids = [id_ for field_name, val in ast.iter_fields(node) if field_name in identifier_field_names]
 		print(ids)
 		get_children_for_node(node)
 
